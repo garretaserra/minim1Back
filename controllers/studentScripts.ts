ad86597dd1cbd94e10faa1532201befaa2b0cd1e
sergi <sergi@unievegroup.com>
2019-11-02 19:01:25 +0100

    Defined functions that need to be coded

---
+++ controllers/studentScripts.ts
@@ -0,0 +1,15 @@
+'use strict';
+export {};
+
+require('../models/Student');
+let mongoose = require('mongoose');
+let student = mongoose.model('Student');
+let ObjectId = require('mongodb').ObjectID;
+
+exports.addStudent = async function (req, res){
+
+};
+
+exports.getStudent = async function (req, res){
+
+};
---
+++ controllers/subjectScripts.ts
@@ -0,0 +1,21 @@
+'use strict';
+export {};
+
+require('../models/Subject');
+let mongoose = require('mongoose');
+let Subject = mongoose.model('Subject');
+let ObjectId = require('mongodb').ObjectID;
+
+exports.addSubject = async function (req, res){
+
+};
+
+exports.addStudentToSubject = async function (req, res){
+
+};
+
+exports.getAllSubjects = async function (req, res){
+
+};
+
+
