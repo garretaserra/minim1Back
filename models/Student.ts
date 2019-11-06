5ccfa032d20663f3014f54ab3448fc3bd18982c9
sergi <sergi@unievegroup.com>
2019-11-06 11:23:13 +0100

    Updated Student model

--- models/Student.ts
+++ models/Student.ts
@@ -5,7 +5,10 @@ let Phone = require('./Phone');
 let student = mongoose.Schema({
     name: String,
     address: String,
-    phones: [Phone]
+    phones: [{
+        description: String,
+        number: String
+    }]
 });
 
 module.exports = mongoose.model('Student', student);
